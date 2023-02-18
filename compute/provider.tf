5dbc73f3fe34eaaeb6f3c7458c83f195e08f7340
collins-new <cmnow22@gmail.com>
2023-02-18 18:04:11 -0500

    Second initial commit

---
+++ compute/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = "~> 1.3.7"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.55.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "access key"
+  secret_key = "secret key"
+}
+
+
+# Create a VPC
+resource "aws_vpc" "projectA-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main"
+  }
+}
